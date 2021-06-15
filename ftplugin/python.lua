3f2ed8169bda50cd452d230f74adfc09ce3be4ec
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2021-06-15 17:18:12 -0400

    Add python quit binding

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -10,6 +10,9 @@ bbind("n", "<localleader>l", [[<Plug>(iron-send-line)]])
 bbind("n", "<localleader>s", [[<Plug>(iron-send-motion)]])
 bbind("n", "<localleader><CR>", [[<Plug>(iron-cr)]])
 bbind("n", "<localleader>x", [[<Plug>(iron-interrupt)]])
+
 bbind("n", "<localleader>rf", [[:IronRepl<CR>]])
+bbind("n", "<localleader>rr", [[:IronRestart<CR>]])
+bbind("n", "<localleader>rq", [[:IronSend! exit()<CR>:IronFocus<CR>:bd!<CR>]])
 
 bbind("v", "<localleader>ss", [[<Plug>(iron-visual-send)]])
