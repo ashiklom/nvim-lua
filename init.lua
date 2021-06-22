d9d8fa6b2896411831d67a75f784b7199587b0c4
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2021-06-22 12:17:41 -0400

    Remove deprecated telescope import

--- init.lua
+++ init.lua
@@ -11,8 +11,6 @@ end
 require('plugins')
 vim.cmd [[autocmd BufWritePost plugins.lua PackerCompile]]
 
--- require('ans-telescope')
-
 -- Settings
 vim.cmd [[filetype plugin indent on]]
 vim.cmd [[syntax enable]]
