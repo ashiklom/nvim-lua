3f1dca44e2e26a38bd7bdd3b0f9adb9966567916
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2021-06-28 14:55:31 -0400

    Cleanup compe

--- lua/ans-compe.lua
+++ lua/ans-compe.lua
@@ -1,5 +1,3 @@
-local vimp = require('vimp')
-
 vim.opt.completeopt = "menuone,noselect"
 
 require"compe".setup {
@@ -16,7 +14,3 @@ require"compe".setup {
     nvim_lua = true;
   }
 }
-
--- local kopts = {silent=true, noremap=true, expr=true}
--- vim.api.nvim_set_keymap("i", "<C-Space>", "compe#complete()", kopts)
--- vim.api.nvim_set_keymap("i", "<CR>", "compe#confirm('<C-y>')", kopts)
