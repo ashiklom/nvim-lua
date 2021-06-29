30180a6b880feb0f77f975e0a2f18c847baa093d
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2021-06-29 09:48:23 -0400

    Fix luadev bindings

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -4,6 +4,6 @@ local function map(mode, lhs, rhs, noremap)
   vim.api.nvim_buf_set_keymap(0, mode, lhs2, rhs, opts)
 end
 
-map('n', 'rf', ':Luadev', true)
+map('n', 'rf', ':Luadev<CR>', true)
 map('n', 'l', '<Plug>(Luadev-RunLine)', false)
-map('v', 'ss', '<Plug>(Luadev-Run)', true)
+map('v', 'ss', '<Plug>(Luadev-Run)', false)
