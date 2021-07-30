060c807544d2d6e7a6b67101960e676c80309036
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2021-07-30 11:41:20 -0400

    Tweak to orgmode config

--- lua/configs/orgmode.lua
+++ lua/configs/orgmode.lua
@@ -1,4 +1,13 @@
 require'orgmode'.setup({
   org_agenda_files = {'~/Dropbox/Notes/**/*'},
-  org_default_notes_file = '~/Dropbox/Notes/unsorted.org'
+  org_default_notes_file = '~/Dropbox/Notes/unsorted.org',
+  mappings = {
+    global = {
+      org_agenda = '<leader>noa',
+      org_capture = '<leader>noc'
+    },
+    capture = {
+      org_capture_finalize = 'ZZ'
+    }
+  }
 })
