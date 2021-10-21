93eefa5b7f72d2eb8f46a5341133bb327a1ccdbc
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2021-10-21 13:02:32 -0400

    Add Nvim-tree setup

--- lua/configs/nvim-tree.lua
+++ lua/configs/nvim-tree.lua
@@ -1,2 +1,4 @@
+require('nvim-tree').setup{}
+
 vim.api.nvim_set_keymap('n', '<leader>fo', [[<Cmd>NvimTreeToggle<CR>]], {silent=true, noremap=true})
 vim.api.nvim_set_keymap('n', '<leader>fr', [[<Cmd>NvimTreeRefresh<CR>]], {silent=true, noremap=true})
