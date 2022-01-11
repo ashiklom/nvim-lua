d78b00569ef92ea7845fc0600986d7b4bf40adab
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2022-01-11 09:11:36 -0500

    Revert -- use custom neogit keys

--- lua/configs/neogit.lua
+++ lua/configs/neogit.lua
@@ -2,10 +2,9 @@ local ng = require('neogit')
 
 ng.setup{
   disable_commit_confirmation = true,
-  use_magit_keybindings = true,
   mappings = {
     status = {
-      -- ["F"] = "PullPopup",
+      ["F"] = "PullPopup",
       ["p"] = "PushPopup"
     }
   }
