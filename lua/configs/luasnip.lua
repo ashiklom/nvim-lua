b8635690a3552a55075c6c38e25d0c6333473060
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2022-04-11 12:22:04 -0400

    Add lightspeed and luasnip

---
+++ lua/configs/luasnip.lua
@@ -0,0 +1,20 @@
+local ok, luasnip = pcall(require, 'luasnip')
+
+if ok then
+  -- require("luasnip.loaders.from_snipmate").lazy_load()
+  require("luasnip.loaders.from_lua").load()
+
+  local t = function(str)
+    return vim.api.nvim_replace_termcodes(str, true, true, true)
+  end
+
+  _G.tab_complete = function()
+    if luasnip.expand_or_jumpable() then
+      return t("<Plug>luasnip-expand-or-jump")
+    else
+      return t("<Tab>")
+    end
+  end
+
+  vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+end
