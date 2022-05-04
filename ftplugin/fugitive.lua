e14a45d699ae37cbe1d037eb2c80036f6e8ff935
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2022-05-04 15:55:37 -0400

    Add fugitive convenience mappings

--- ftplugin/fugitive.lua
+++ ftplugin/fugitive.lua
@@ -1,1 +1,12 @@
+-- Magit mappings make a lot of sense.
+-- Trying to reproduce them here.
+-- https://magit.vc/manual/magit/
+vim.keymap.set('n', 'bc', [[:Git checkout ]], {buffer=true})
+
+vim.keymap.set('n', 'm', [[:Git merge ]], {buffer=true})
+
 vim.keymap.set('n', 'pp', [[:Git push<CR>]], {buffer=true})
+vim.keymap.set('n', 'pu', [[:Git push upstream<CR>]], {buffer=true})
+
+vim.keymap.set('n', 'Fm', [[:Git pull<CR>]], {buffer=true})
+vim.keymap.set('n', 'Fu', [[:Git pull upstream<CR>]], {buffer=true})
