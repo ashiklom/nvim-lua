99b93320c24392b8f8eda205ff602273a9fb1ee9
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2022-05-04 15:11:43 -0400

    Update cmp configuration to match kickstart

--- lua/configs/cmp.lua
+++ lua/configs/cmp.lua
@@ -1,17 +1,26 @@
 vim.opt.completeopt = "menu,menuone,noselect"
 
 local cmp = require("cmp")
+local luasnip = require('luasnip')
 
 cmp.setup {
   completion = { autocomplete = false },
-  mapping = {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end
+  },
+  mapping = cmp.mapping.preset.insert({
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-n>'] = cmp.mapping.select_next_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<CR>'] = cmp.mapping.confirm({behavior = cmp.ConfirmBehavior.Replace, select = true})
-  },
+    ['<CR>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    })
+  }),
   sources = {
     { name = 'nvim_lsp' },
     { name = 'luasnip' }
