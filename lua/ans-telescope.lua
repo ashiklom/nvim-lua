d8e651a5a97e15a57ddd0677b2e4974425c5b9ea
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2022-05-09 11:24:06 -0400

    Remove unused telescope functions

--- lua/ans-telescope.lua
+++ lua/ans-telescope.lua
@@ -3,18 +3,10 @@ telescope.load_extension('project')
 telescope.load_extension('file_browser')
 telescope.load_extension('fzf')
 
-local tb = require('telescope.builtin')
 local tfb = telescope.extensions.file_browser
-local path = require('plenary.path')
 local utils = require('ans-utils')
 
 local M = {}
-
-function M.fd_files()
-  local opts = {find_command = {"fd", "--type", "file"}}
-  local hasgit = pcall(tb.git_files, opts)
-  if not hasgit then tb.find_files(opts) end
-end
 
 function M.file_browser_cwd()
   local cwd = utils.buf_cwd()
