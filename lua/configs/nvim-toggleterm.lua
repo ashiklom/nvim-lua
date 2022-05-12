5cc6ffd306739a1497dc8ed868d758947a84b5e6
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2022-05-12 17:02:10 -0400

    Improve toggleterm keybindings

--- lua/configs/nvim-toggleterm.lua
+++ lua/configs/nvim-toggleterm.lua
@@ -1,5 +1,17 @@
-require('toggleterm').setup{
+local tt = require('toggleterm')
+
+tt.setup{
   open_mapping = [[<c-\>]],
   direction = "float",
   hide_numbers = true
 }
+
+local function set_term_keymaps()
+  local opts = {buffer=true}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+end
+
+vim.api.nvim_create_autocmd({"TermOpen"}, {
+  pattern = {"term://*"},
+  callback = set_term_keymaps
+})
