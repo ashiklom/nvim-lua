43a59b303e33034d58039778cd8e6ff3e6931de1
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2022-05-25 11:28:08 -0500

    Use telescope for git branches

--- ftplugin/fugitive.lua
+++ ftplugin/fugitive.lua
@@ -1,8 +1,6 @@
 -- Magit mappings make a lot of sense.
 -- Trying to reproduce them here.
 -- https://magit.vc/manual/magit/
-vim.keymap.set('n', 'bc', [[:Git checkout ]], {buffer=true})
-
 vim.keymap.set('n', 'm', [[:Git merge ]], {buffer=true})
 
 vim.keymap.set('n', 'pp', [[:Git push<CR>]], {buffer=true})
@@ -15,3 +13,8 @@ vim.keymap.set('n', 'Fu', [[:Git pull upstream<CR>]], {buffer=true})
 vim.keymap.set('n', 'q', [[:quit<CR>]], {buffer=true, silent=true})
 
 vim.keymap.set('n', '<TAB>', '=', {buffer=true, remap=true})
+
+local ok, tb = pcall(require, 'telescope.builtin')
+if ok then
+  vim.keymap.set('n', 'bc', tb.git_branches, {buffer=true})
+end
