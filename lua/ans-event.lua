331303a5d5bf6497d3707222ba0ccb77e281974a
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2022-05-31 13:07:06 -0400

    Resize vim windows on screen size change

--- lua/ans-event.lua
+++ lua/ans-event.lua
@@ -4,6 +4,11 @@ utils.nvim_create_augroup('Spelling', {
   {"FileType", "text,tex,markdown,plaintex,gitcommit,org", "setlocal spell wrap"}
 })
 
+-- Auto-resize window splits when vim screen changes size
+utils.nvim_create_augroup('Resize', {
+  {"VimResized", "*", "wincmd ="}
+})
+
 -- Automatically change window directory on file enter
 -- utils.nvim_create_augroup("Chdir", {
 --   {"BufEnter", "*", [[if g:fix_wd == 0 | execute 'lcd '.v:lua.git_cwd() | endif]]}
