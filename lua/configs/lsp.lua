d4602b99e900f3cf3e2ecd414278cabc34c52171
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2022-06-25 11:20:33 -0400

    Update LSP configuration

--- lua/configs/lsp.lua
+++ lua/configs/lsp.lua
@@ -1,10 +1,13 @@
 local lsp_installer = require("nvim-lsp-installer")
+local lspconfig = require('lspconfig')
+
+lsp_installer.setup{}
 -- local lsp_insatller_servers = require('nvim-lsp-installer.servers')
 
 local on_attach = function(_, bufnr)
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
   local nmap = function(lhs, rhs)
-    local opts = { noremap=true, silent=true, buffer=bufnr }
+    local opts = { silent=true, buffer=bufnr }
     vim.keymap.set('n', lhs, rhs, opts)
   end
   nmap('gd', vim.lsp.buf.definition)
@@ -23,15 +26,20 @@ if ok then
   capabilities = cmp_nl.update_capabilities(capabilities)
 end
 
-lsp_installer.on_server_ready(function(server)
-  local opts = {
-    on_attach = on_attach,
-    capabilities = capabilities
-  }
-  if server.name == "sumneko_lua" then
-    opts.settings = {Lua = {diagnostics = {globals = {"vim"}}}}
-  elseif server.name == "bashls" then
-    opts.filetypes = {"sh", "zsh"}
-  end
-  server:setup(opts)
-end)
+lspconfig['r_language_server'].setup{
+  on_attach = on_attach,
+  capabilities = capabilities
+}
+
+-- for index, server in ipairs(lsp_installer.get_installed_servers()) do
+--   local opts = {
+--     on_attach = on_attach,
+--     capabilities = capabilities
+--   }
+--   if server.name == "sumneko_lua" then
+--     opts.settings = {Lua = {diagnostics = {globals = {"vim"}}}}
+--   elseif server.name == "bashls" then
+--     opts.filetypes = {"sh", "zsh"}
+--   end
+--   lspconfig[server].setup(opts)
+-- end
