da1a7dfa2f3fa940decdaa5a1fbdd806fe311f4e
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2022-06-27 21:00:58 -0400

    Use R repl without bracketed paste

--- lua/configs/iron.lua
+++ lua/configs/iron.lua
@@ -4,6 +4,7 @@ iron.setup{config = {
   repl_open_cmd = 'belowright 15 split',
   repl_definition = {
     python = require('iron.fts.python').ipython,
+    r = {command = {"R"}},
     julia = {command = {"julia"}}
   }
 }}
