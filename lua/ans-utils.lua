be4c33ca073717a26290f053de810953950ce419
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2022-07-01 14:11:17 -0400

    Make plenary stuff in utils conditional

--- lua/ans-utils.lua
+++ lua/ans-utils.lua
@@ -1,5 +1,3 @@
-local path = require('plenary.path')
-
 local utils = {}
 
 function utils.nvim_create_augroup(group_name, definition)
@@ -16,36 +14,44 @@ function utils.buf_name()
   return vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf())
 end
 
-function utils.git_cwd()
-  local bufname = utils.buf_name()
-  local buffer = path:new(bufname)
-  local cwd
-  if bufname == '' or string.find(bufname, 'term:') then
-    cwd = tostring(vim.fn["getcwd"]())
-  elseif buffer:is_dir() then
-    cwd = tostring(buffer)
-  else
-    cwd = tostring(buffer:parent())
+local ok, _ = pcall(require, 'plenary')
+if ok then
+  local path = require('plenary.path')
+  function utils.git_cwd()
+    local bufname = utils.buf_name()
+    local buffer = path:new(bufname)
+    local cwd
+    if bufname == '' or string.find(bufname, 'term:') then
+      cwd = tostring(vim.fn["getcwd"]())
+    elseif buffer:is_dir() then
+      cwd = tostring(buffer)
+    else
+      cwd = tostring(buffer:parent())
+    end
+    local result = vim.fn.systemlist(string.format("git -C %s rev-parse --show-toplevel", cwd))[1]
+    if string.find(result, "fatal") then
+      return cwd
+    else
+      return result
+    end
   end
-  local result = vim.fn.systemlist(string.format("git -C %s rev-parse --show-toplevel", cwd))[1]
-  if string.find(result, "fatal") then
-    return cwd
-  else
-    return result
-  end
-end
 
-function utils.buf_cwd()
-  local bufname = utils.buf_name()
-  if bufname == '' then
-    return vim.fn["getcwd"]()
+  function utils.buf_cwd()
+    local bufname = utils.buf_name()
+    if bufname == '' then
+      return vim.fn["getcwd"]()
+    end
+    local buffer = path:new(bufname)
+    if buffer:is_dir() then
+      return tostring(buffer)
+    end
+    local cwd = buffer:parent()
+    return tostring(cwd)
   end
-  local buffer = path:new(bufname)
-  if buffer:is_dir() then
-    return tostring(buffer)
-  end
-  local cwd = buffer:parent()
-  return tostring(cwd)
+
+  _G.buf_cwd = utils.buf_cwd
+  _G.git_cwd = utils.git_cwd
+
 end
 
 function utils.reload_vimrc()
@@ -58,8 +64,5 @@ function utils.reload_vimrc()
   vim.cmd [[luafile ~/.config/nvim/init.lua]]
   print("Reloaded vimrc!")
 end
-
-_G.buf_cwd = utils.buf_cwd
-_G.git_cwd = utils.git_cwd
 
 return utils
