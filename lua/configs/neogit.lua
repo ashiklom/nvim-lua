9fd83293206bf702b052d648354ff0ce9e8c246a
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2022-08-31 10:16:21 -0400

    Split neogit above

--- lua/configs/neogit.lua
+++ lua/configs/neogit.lua
@@ -1,6 +1,6 @@
 local neogit = require('neogit')
 
-vim.keymap.set('n', '<leader>gg', function() neogit.open({ kind="split"}) end, {silent=true})
+vim.keymap.set('n', '<leader>gg', function() neogit.open({ kind="split_above"}) end, {silent=true})
 
 neogit.setup{
   disable_commit_confirmation = true,
