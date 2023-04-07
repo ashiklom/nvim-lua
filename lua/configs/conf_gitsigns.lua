7896a31e3e0511c693f4642504ccabeb1c4e2811
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2023-04-07 14:06:09 -0400

    Document conf_gitsigns keybindings

--- lua/configs/conf_gitsigns.lua
+++ lua/configs/conf_gitsigns.lua
@@ -2,12 +2,12 @@ local gs = require('gitsigns')
 
 gs.setup{
   on_attach = function()
-    vim.keymap.set("n", "]h", gs.next_hunk)
-    vim.keymap.set("n", "[h", gs.prev_hunk)
-    vim.keymap.set({"n", "v"}, "<leader>hh", gs.preview_hunk)
-    vim.keymap.set({"n", "v"}, "<leader>hs", gs.stage_hunk)
-    vim.keymap.set({"n", "v"}, "<leader>hx", gs.reset_hunk)
-    vim.keymap.set({"n", "v"}, "<leader>hz", gs.undo_stage_hunk)
-    vim.keymap.set("n", "<leader>hb", gs.blame_line)
+    vim.keymap.set("n", "]h", gs.next_hunk, {desc="Next hunk"})
+    vim.keymap.set("n", "[h", gs.prev_hunk, {desc="Previous hunk"})
+    vim.keymap.set({"n", "v"}, "<leader>hh", gs.preview_hunk, {desc="Preview hunk"})
+    vim.keymap.set({"n", "v"}, "<leader>hs", gs.stage_hunk, {desc="Stage hunk"})
+    vim.keymap.set({"n", "v"}, "<leader>hx", gs.reset_hunk, {desc="Reset hunk"})
+    vim.keymap.set({"n", "v"}, "<leader>hz", gs.undo_stage_hunk, {desc="Undo stage hunk"})
+    vim.keymap.set("n", "<leader>hb", gs.blame_line, {desc="Blame line"})
   end
 }
