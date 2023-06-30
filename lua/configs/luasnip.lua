7266cfe0c4f5ef0b8c0e9bd587f2ea74012bf930
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2023-06-30 10:50:19 -0400

    Fix luasnip tab handling
    This fixes the annoying Julia delay issue.

--- lua/configs/luasnip.lua
+++ lua/configs/luasnip.lua
@@ -4,17 +4,14 @@ if ok then
   require("luasnip.loaders.from_lua").lazy_load()
   require("luasnip.loaders.from_snipmate").lazy_load()
 
-  local t = function(str)
-    return vim.api.nvim_replace_termcodes(str, true, true, true)
-  end
-
-  _G.tab_complete = function()
+  local function tab_complete()
     if luasnip.expand_or_jumpable() then
-      return t("<Plug>luasnip-expand-or-jump")
+      return luasnip.expand_or_jump()
     else
-      return t("<Tab>")
+      local tab = vim.api.nvim_replace_termcodes('<Tab>', true, true, true)
+      return vim.api.nvim_feedkeys(tab, 't', true)
     end
   end
 
-  vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+  vim.keymap.set("i", "<Tab>", tab_complete)
 end
