b49a79f13693a9d8055b0f4cf1ef0adf50c71824
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2024-03-21 14:10:56 -0400

    Describe Julia keybindings

--- after/ftplugin/julia.lua
+++ after/ftplugin/julia.lua
@@ -7,28 +7,29 @@ vim.g.julia_indent_align_funcargs = 0
 vim.g.julia_indent_align_brackets = 0
 vim.g.julia_indent_align_import = 1
 
-local function bind(mode, map, command)
-  vim.keymap.set(mode, map, command, {silent=true, buffer=true})
+local function bind(mode, map, command, desc)
+  vim.keymap.set(mode, map, command, {silent=true, buffer=true, desc=desc})
 end
 
-bind('n', '<localleader>rf', [[:IronRepl<CR>]])
-bind('n', '<localleader>rr', iron.repl_restart)
-bind('n', '<localleader>rq', iron.close_repl)
-bind('n', '<localleader>r.', function() iron.send(nil, 'import Pkg; Pkg.activate(".")') end)
+bind('n', '<localleader>rf', [[:IronRepl<CR>]], "Start REPL")
+bind('n', '<localleader>rr', iron.repl_restart, "Restart REPL")
+bind('n', '<localleader>rq', iron.close_repl, "Quit REPL")
+bind('n', '<localleader>r.', function() iron.send(nil, 'import Pkg; Pkg.activate(".")') end, "Activate package in CWD")
 
-bind("n", "<localleader>l", iron.send_line)
+bind("n", "<localleader>l", iron.send_line, "Send line")
 -- bind("n", "<localleader>s", [[<Plug>(iron-send-motion)]])
 local ctrlc = string.char(03)
-bind('n', '<localleader>x', function() iron.send(nil, ctrlc) end)
-bind('v', '<localleader>ss', iron.visual_send)
-bind('n', '<localleader>aa', iron.send_file)
+bind('n', '<localleader>x', function() iron.send(nil, ctrlc) end, "Send C-C")
+bind('v', '<localleader>ss', iron.visual_send, "Send selection")
+bind('n', '<localleader>aa', iron.send_file, "Send file")
 
 -- Send current word (produces better formatting than "print")
 bind('n', '<localleader>rp', function()
   iron.send(nil, {vim.fn.expand('<cword>')})
-end)
+end, "Send word")
 bind('n', '<localleader>rP', function()
   local cmd = string.format('print(%s)', vim.fn.expand('<cword>'))
   iron.send(nil, {cmd})
-end)
-bind('n', '<localleader>rz', function() iron.send(nil, 'isinteractive()') end)
+end, "Print word")
+bind('n', '<localleader>rz', function() iron.send(nil, 'isinteractive()') end,
+  "Is interactive?")
