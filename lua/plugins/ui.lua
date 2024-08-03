45f14c6e3e0d78c5cc90204564a7db470e67cb5e
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2024-08-03 16:26:23 -0400

    Improve nvim-notify config

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -26,6 +26,26 @@ return {
   },
 
   {
+    "rcarriga/nvim-notify",
+    keys = {
+      { "<leader>snD", function() require('notify').dismiss({ silent = true, pending = true }) end }
+    },
+    opts = {
+      stages = "static", -- No animation
+      timeout = 3000,
+      max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.75)
+      end,
+      on_open = function(win)
+        vim.api.nvim_win_set_config(win, { zindex = 100 })
+      end,
+    }
+  },
+
+  {
     'stevearc/oil.nvim',
     config = true,
     dependencies = { 'nvim-tree/nvim-web-devicons' }
