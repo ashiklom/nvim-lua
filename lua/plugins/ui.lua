78ded5a98c9b84eb9d9c4a9a0593044c4f844072
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2024-08-08 08:20:01 -0400

    Disable automatic noice signature

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -8,8 +8,10 @@ return {
         override = {
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
           ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = false, -- requires hrsh7th/nvim-cmp
+          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
         },
+        -- Disable auto documentation popup
+        signature = {auto_open = {enabled = false}}
       },
       routes = {
         {
