ec78aeef6625a122fa29dfd358b31499011d636c
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2024-08-15 10:41:11 -0400

    Fix rmd ftplugin

--- after/ftplugin/rmd.lua
+++ after/ftplugin/rmd.lua
@@ -1,4 +1,4 @@
-local rconfig = assert(loadfile(vim.fn.stdpath("config") .. "/ftplugin/r.lua"))
+local rconfig = assert(loadfile(vim.fn.stdpath("config") .. "/after/ftplugin/r.lua"))
 rconfig()
 
 vim.api.nvim_win_set_option(0, "wrap", true)
