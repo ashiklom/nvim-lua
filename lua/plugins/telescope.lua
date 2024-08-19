8772f5922847efe492529afdf19c9814545e4953
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2024-08-19 12:09:41 -0400

    Telescope-bibtex configuration

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,14 +12,14 @@ return {
   {'nvim-telescope/telescope-project.nvim'},
   {'nvim-telescope/telescope-file-browser.nvim'},
   {
-    'ashiklom/telescope-bibtex.nvim',
+    'nvim-telescope/telescope-bibtex.nvim',
     config = function()
       local tele = require("telescope")
       tele.load_extension("bibtex")
       tele.setup{
         extensions = {
           bibtex = {
-            global_files = {"/home/ashiklom/zotero-library.bib"}
+            global_files = {"/home/ashiklom/windows-home/Documents/zotero-references.bib"}
           }
         }
       }
