144573ae103d4044b6237d123db074c0caf7c391
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2024-09-26 13:03:12 -0400

    Add SSH clipboard config

--- lua/ans-opts.lua
+++ lua/ans-opts.lua
@@ -46,3 +46,23 @@ if exists('g:vv')
   VVset fontfamily=JuliaMono,nonicons,Hack\ Nerd\ Font
 endif
 ]])
+
+-- Stolen from TJ Devries:
+-- https://github.com/tjdevries/config.nvim/blob/master/plugin/clipboard.lua
+if vim.env.SSH_CONNECTION then
+  local function vim_paste()
+    local content = vim.fn.getreg('"')
+    return vim.split(content, "\n")
+  end
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*")
+    },
+    paste = {
+      ["+"] = vim_paste,
+      ["*"] = vim_paste
+    }
+  }
+end
