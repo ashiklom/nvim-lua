ac64fb066e5e2ea3e8dbcfc2dc7be843dd860436
Alexey Shiklomanov <alexey.shiklomanov@nasa.gov>
2025-04-21 15:33:21 -0400

    Mimic lazy config for mini.ai

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -8,51 +8,5 @@ tconfig.setup{
   highlight = {
     enable=true,
     disable={"r"}
-  },
-  textobjects = {
-    select = {
-      enable = true,
-      keymaps = {
-        ["af"] = "@function.outer",
-        ["if"] = "@function.inner",
-        ["ac"] = "@class.outer",
-        ["ic"] = "@class.inner",
-        ["aa"] = "@parameter.outer",
-        ["ia"] = "@parameter.inner"
-      }
-    },
-    move = {
-      enable = true,
-      set_jumps = true,
-      goto_next_start = {
-        [']f'] = '@function.outer',
-        [']c'] = '@class.outer',
-        [']a'] = '@parameter.outer'
-      },
-      goto_next_end = {
-        [']F'] = '@function.outer',
-        [']C'] = '@class.outer',
-        [']A'] = '@parameter.outer'
-      },
-      goto_previous_start = {
-        ['[f'] = '@function.outer',
-        ['[c'] = '@class.outer',
-        ['[a'] = '@parameter.outer'
-      },
-      goto_previous_end = {
-        ['[F'] = '@function.outer',
-        ['[C'] = '@class.outer',
-        ['[A'] = '@parameter.outer'
-      }
-    },
-    swap = {
-      enable = true,
-      swap_next = {
-        ["]x"] = "@parameter.inner"
-      },
-      swap_previous = {
-        ["[x"] = "@parameter.inner"
-      }
-    }
   }
 }
--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -47,12 +47,29 @@ return {
   -- Fallbacks for argument selection and exchange
   {
     'echasnovski/mini.ai',
-    opts = {
-      mappings = {
-        goto_left = 'gs[',
-        goto_right = 'gs]'
+    opts = function()
+      local ai = require("mini.ai")
+      return {
+        n_lines = 500,
+        custom_textobjects = {
+          o = ai.gen_spec.treesitter({ -- code block
+            a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+            i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+          }),
+          f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
+          c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
+          t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
+          d = { "%f[%d]%d+" }, -- digits
+          e = { -- Word with case
+            { "%u[%l%d]+%f[^%l%d]", "%f[%S][%l%d]+%f[^%l%d]", "%f[%P][%l%d]+%f[^%l%d]", "^[%l%d]+%f[^%l%d]" },
+            "^().*()$",
+          },
+          -- g = LazyVim.mini.ai_buffer, -- buffer
+          u = ai.gen_spec.function_call(), -- u for "Usage"
+          U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot in function name
+        }
       }
-    }
+    end,
   },
   {
     "gbprod/substitute.nvim",
