9eb7a905717f79ab19a8f5041bb8f11594ab6491
Alexey Shiklomanov <alexey.shiklomanov@nasa.gov>
2025-04-22 09:59:11 -0400

    Replace friendly-snippets with scissors
    For custom snippets.

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,12 +1,20 @@
 return {
   {
     "saghen/blink.cmp",
-    dependencies = {
-      "rafamadriz/friendly-snippets"
-    },
-    version = '1.*',
+    dependencies = { "chrisgrieser/nvim-scissors" },
+    version = "1.*",
     opts = {
-      keymap = { preset = "enter" }
+      keymap = { preset = "enter" },
+    },
+  },
+  {
+    "chrisgrieser/nvim-scissors",
+    opts = {
+      snippetDir = vim.fn.stdpath("config") .. "/snippets",
+    },
+    keys = {
+      {"<leader>ne", function() require('scissors').editSnippet() end, desc = "Edit snippet"},
+      {"<leader>na", function() require('scissors').addNewSnippet() end, mode = {"n", "x"}, desc = "Add new snippet"},
     }
-  }
+  },
 }
