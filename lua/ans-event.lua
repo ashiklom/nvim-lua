6003bdee853911c71088bd223d129575f97250a2
Alexey Shiklomanov <alexey.shiklomanov@nasa.gov>
2025-05-14 10:18:59 -0400

    Add grug-far to quit-with-q

--- lua/ans-event.lua
+++ lua/ans-event.lua
@@ -30,7 +30,8 @@ vim.api.nvim_create_autocmd("FileType", {
     "lspinfo",
     "notify",
     "qf",
-    "checkhealth"
+    "checkhealth",
+    "grug-far"
   },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
