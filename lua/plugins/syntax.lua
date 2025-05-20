156b690da981d8737f7e995e1303f970744861ea
Alexey Shiklomanov <alexey.shiklomanov@nasa.gov>
2025-05-20 13:04:37 -0400

    Replace csv with csvview

--- lua/plugins/syntax.lua
+++ lua/plugins/syntax.lua
@@ -8,5 +8,22 @@ return {
   { 'mracos/mermaid.vim', ft = {"mermaid"} },
   { 'eigenfoo/stan-vim' },
 
-  { 'chrisbra/csv.vim', ft = {'csv'} }
+  {
+    'hat0uma/csvview.nvim',
+    ft = {'csv'},
+    opts = {
+      keymaps = {
+        textobject_field_inner = {"if", mode = {"o", "x"}},
+        textobject_field_outer = {"af", mode = {"o", "x"}},
+        jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
+        jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
+        jump_next_row = { "<Enter>", mode = { "n", "v" } },
+        jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
+      }
+    },
+    keys = {
+      {"<localleader>t", function() require('csvview').toggle() end, mode = {"n", "x"}, desc = "Toggle CSVview"}
+    },
+    cmd = { "CsvViewEnable", "CsvViewDisable", "CSvViewToggle" }
+  }
 }
