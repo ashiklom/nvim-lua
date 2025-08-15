7aa97299782f82bddc4aba90197032017cee4938
Alexey Shiklomanov <alexey.shiklomanov@nasa.gov>
2025-08-15 16:35:45 -0400

    Manual leap mappings
    ...so that "S" doesn't conflict with surround.

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -3,18 +3,14 @@ return {
     "ggandor/leap.nvim",
     enabled = true,
     keys = {
-      { "s", mode = { "n", "x", "o" }, desc = "Leap Forward to" },
-      { "S", mode = { "n", "x", "o" }, desc = "Leap Backward to" },
-      { "gs", mode = { "n", "x", "o" }, desc = "Leap from Windows" },
+      { "s", mode = { "n" }, desc = "Leap Forward to" },
+      { "S", mode = { "n" }, desc = "Leap Backward to" },
+      { "gs", mode = { "n" }, desc = "Leap from Windows" },
     },
-    config = function(_, opts)
-      local leap = require("leap")
-      for k, v in pairs(opts) do
-        leap.opts[k] = v
-      end
-      leap.add_default_mappings(true)
-      vim.keymap.del({ "x", "o" }, "x")
-      vim.keymap.del({ "x", "o" }, "X")
+    config = function()
+      vim.keymap.set("n", "s", "<Plug>(leap-forward)", {silent = true, desc = "Leap forward"})
+      vim.keymap.set("n", "S", "<Plug>(leap-backward)", {silent = true, desc = "Leap backward"})
+      vim.keymap.set("n", "gs", "<Plug>(leap-from-window)", {silent = true, desc = "Leap from window"})
     end,
   },
 
