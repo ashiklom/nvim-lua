3760671c368ca40399fb3fa1f1826279cfc4caad
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2026-06-12 13:53:24 -0400

    fix nvim-ception with man pages
    closes #17.

--- after/ftplugin/sh.lua
+++ after/ftplugin/sh.lua
@@ -1,2 +1,6 @@
 local utils = require('ans-utils')
 utils.register_iron_keys('echo $%s')
+
+-- Fix nvim-ception for man pages (triggered by `K`) by just calling :Man 
+-- command directly. See GH issue #17.
+vim.bo.keywordprg = ":Man"
