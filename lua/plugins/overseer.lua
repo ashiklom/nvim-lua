efbb5795c0c37cabfdc7bfe2a3429cd400abd1c7
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2026-07-06 13:26:12 -0400

    overseer restart task binding

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -4,5 +4,21 @@ return {
     ---@module 'overseer'
     ---@type overseer.SetupOpts
     opts = {},
+    config = function ()
+      local ovs = require('overseer')
+      vim.keymap.set("n", "<leader>ot", function() ovs.toggle() end, {desc = "Overseer toggle"})
+      vim.keymap.set("n", "<leader>or", function() ovs.run_task() end, {desc = "Overseer run"})
+
+      local overseer_rerun = function()
+        local tasks = ovs.list_tasks({recent_first = true})
+        if vim.tbl_isempty(tasks) then
+          vim.notify("No tasks found")
+          return
+        end
+        local task = tasks[1]
+        ovs.run_action(task, "restart")
+      end
+      vim.keymap.set("n", "<leader>oo", overseer_rerun, {desc = "Overseer rerun"})
+    end
   },
 }
